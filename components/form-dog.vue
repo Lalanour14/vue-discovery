ab5149605057e48e1e94fed7abaf507275d33daa
Salim <bouzidsezlim@gmail.com>
2023-07-24 16:40:19 +0200

    emit + post

--- components/form-dog.vue
+++ components/form-dog.vue
@@ -1,8 +1,30 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Dog } from '../entities';
+
+const emit = defineEmits(['submitDog']);
+
+const dog = ref<Dog>({name:"",breed:"",birthdate:""});
+
+ function handleSubmit(){
+  console.log(dog);
+  emit('submitDog',dog.value);
+
+ }
+
+
+</script>
 
 <template>
   <div>
-    Component: form-dog
+  <form @submit.prevent="handleSubmit()">
+    <label for="name">Name</label>
+   <input type="text" v-model="dog.name"  id="name">
+   <label for="breed">Breed</label>
+   <input type="text" v-model="dog.breed" id="breed">
+   <label for="name">birthdate</label>
+   <input type="text" v-model="dog.birthdate"  id="birthdate">
+    <button >Submit</button>
+  </form>
   </div>
 </template>
 
