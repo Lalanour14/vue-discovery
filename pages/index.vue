6993552b1898d25cb652ebe9e2d34edc5858a2a4
Salim <bouzidsezlim@gmail.com>
2023-07-24 14:01:53 +0200

    first

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const first = ref('test');
+ function changeFirst (){
+
+  first.value = 'autre chose';
+ }
+
+
+</script>
+
+
+<template>
+  <div>
+    {{ first }}
+
+  </div>
+  <button @click='changeFirst()'>click</button>
+</template>
+
+<style scoped></style>
---
+++ pages/name-list.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const names = ref(['fifi','riri','loulou','lili']);
+const newName = ref('');
+function addName(){
+  names.value.push(newName.value) ;
+}
+
+</script>
+<template>
+  <div>
+    <input type="text" v-model="newName" placeholder="type a name ...">
+    <button @click="addName()"> Add</button>
+  <ul>
+    <li v-for=" item of names">{{item}}</li>
+    
+  </ul>
+  </div>
+</template>
+
+<style scoped></style>
